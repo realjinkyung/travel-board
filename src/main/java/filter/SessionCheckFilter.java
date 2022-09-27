057bfa46fffa7e10bfe2f8ec08a1447082cd938f
liverbird <ehdgns07@gmail.com>
2022-09-27 15:02:28 +0900

    sessioncheck filter fix

--- src/main/java/filter/SessionCheckFilter.java
+++ src/main/java/filter/SessionCheckFilter.java
@@ -18,44 +18,44 @@ import javax.servlet.http.HttpSession;
 /**
  * Servlet Filter implementation class SessionCheckFIlter
  */
-@WebFilter(filterName="/SessionCheckFilter", urlPatterns="*")
+@WebFilter(filterName = "/SessionCheckFilter", urlPatterns = "*")
 public class SessionCheckFilter implements Filter {
 
-	/**
-	 * @see Filter#destroy()
-	 */
-	public void destroy() {
-		// TODO Auto-generated method stub
-	}
-	
-	List<String> whiteList= new ArrayList<>();
+    /**
+     * @see Filter#destroy()
+     */
+    public void destroy() {
+        // TODO Auto-generated method stub
+    }
 
-	/**
-	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
-	 */
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-		HttpServletRequest req = (HttpServletRequest) request;
-		HttpSession session = req.getSession(false);
-		String uri = whiteList.stream().filter((uriItem)-> uriItem.equals(req.getServletPath())).findFirst().orElse(null);
+    List<String> whiteList = new ArrayList<>();
 
-		if(Objects.isNull(uri)){
-			if(Objects.isNull(session) || Objects.isNull(session.getAttribute("username"))) {
-				req.getRequestDispatcher("/login.jsp").forward(request, response);
-		}
-		}
-		
-		chain.doFilter(request, response);
-	}
+    /**
+     * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+     */
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpSession session = req.getSession(false);
+        String uri = whiteList.stream().filter((uriItem) -> uriItem.equals(req.getServletPath())).findFirst().orElse(null);
 
-	/**
-	 * @see Filter#init(FilterConfig)
-	 */
-	public void init(FilterConfig fConfig) throws ServletException {
-		// TODO Auto-generated method stub
-		whiteList.add("/joinForm.jsp");
-		whiteList.add("/login.do");
+        if (Objects.isNull(uri)) {
+            if (Objects.isNull(session) || Objects.isNull(session.getAttribute("username"))) {
+                req.getRequestDispatcher("/login.jsp").forward(request, response);
+            }
+        }
+
+        chain.doFilter(request, response);
+    }
+
+    /**
+     * @see Filter#init(FilterConfig)
+     */
+    public void init(FilterConfig fConfig) throws ServletException {
+        // TODO Auto-generated method stub
+        whiteList.add("/joinForm.jsp");
+        whiteList.add("/login.do");
 
 
-	}
+    }
 
 }
